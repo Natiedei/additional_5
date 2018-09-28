e38d157e7ca5036fda5dcbddda5438f3ecd5f135
kate.d <decpumes@gmail.com>
2018-09-28 21:19:21 +0300

    not done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  //var Skob = str.split('');
+  //if(str.length % 2 == 1) return false; else return true;
+
+  var check = [];
+  for (let i = 0; i < str.length; i++) 
+  {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) check.push(str[i]);
+      if (str[i] == bracketsConfig[j][1]) check.pop(str[i]);  
+     
+    }
+  }
+  if (check == 0) return true;
+  else return false;
+
+ 
 }
